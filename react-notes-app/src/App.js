c9bf21289f748f8bfa0d34cc08b9b2c4e3841c4e
th0mps0nty <th0mps0n.ty@protonmail.com>
2018-02-13 17:41:22 -0500

    second commit stopped before styling

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Note from './Note/Note';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+// We're going to setup the React state of our component
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" },
+      ],
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React & Firebase To-Do List</div>
+        </div>
+        <div className="notesBody">
+        {
+          this.state.notes.map((note) => {
+            return (
+              <Note noteContent={note.noteContent} noteId={note.Id} key={note.Id} />
+            )
+          })
+        }
+        </div>
+        <div className="notesFooter">
+          Footer Will Go Here...
+        </div>
       </div>
     );
   }
